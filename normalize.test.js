eb48c695b7ee5e80535b62b4874cc5314109e1ac
surmavagit <georgesurmava@gmail.com>
2023-09-17 15:52:34 +0200

    add 3 more tests

--- normalize.test.js
+++ normalize.test.js
@@ -5,3 +5,16 @@ test('normalize url starting with http drops the scheme', () => {
     expect(normalizeUrl('http://example.com')).toBe('example.com');
 });
 
+test('normalize url starting with https drops the scheme', () => {
+    expect(normalizeUrl('https://example.com')).toBe('example.com');
+});
+
+test('normalize url ending with slash drops the slash', () => {
+    expect(normalizeUrl('example.com/')).toBe('example.com');
+});
+
+test('normalize an already normalized url returns the url', () => {
+    const correctUrl = 'example.com/words/something?query=query#fragment';
+    expect(normalizeUrl(correctUrl)).toBe(correctUrl);
+});
+
