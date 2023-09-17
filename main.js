95cc9919622e8af19dbd680bc9905fd371ce0065
surmavagit <georgesurmava@gmail.com>
2023-09-17 22:58:54 +0200

    add main function taking cli argument

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-console.log('hello world')
+const argIndex = 2;
+
+function main() {
+    if (process.argv.length < argIndex + 1) {
+        throw new Error("command line argument not provided");
+    } else if (process.argv.length > argIndex + 1) {
+        throw new Error("too many command line arguments");
+    }
+    const baseURL = process.argv[argIndex];
+    console.log(`Crawler starts at ${baseURL}`)
+}
+
+main();
 
